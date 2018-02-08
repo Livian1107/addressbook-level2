5b0c4f75bde7fff6c3346ad4f463ccc2875ac4f9
Livian1107 <e0134124@u.nus.edu>
2018-02-08 23:41:00 +0800

    add a class PostCode.

--- src/seedu/addressbook/data/person/PostCode.java
+++ src/seedu/addressbook/data/person/PostCode.java
@@ -1,4 +1,17 @@
 package seedu.addressbook.data.person;
 
 public class PostCode {
+    private final String postcode;
+
+    public PostCode(String pos){
+        this.postcode = pos;
+    }
+
+    public boolean isInvalid(){
+        return postcode == null;
+    }
+
+    public String getPos() {
+        return postcode;
+    }
 }
