f430ec959b09e2053ccd8baacaea03e9e24ea11e
Livian1107 <e0134124@u.nus.edu>
2018-02-08 23:42:00 +0800

    add a class Block

--- src/seedu/addressbook/data/person/Block.java
+++ src/seedu/addressbook/data/person/Block.java
@@ -1,4 +1,17 @@
 package seedu.addressbook.data.person;
 
 public class Block {
+    private final String block;
+
+    public Block(String blk){
+        this.block = blk;
+    }
+
+    public boolean isInvalid(){
+        return block == null;
+    }
+
+    public String getBlock() {
+        return block;
+    }
 }
