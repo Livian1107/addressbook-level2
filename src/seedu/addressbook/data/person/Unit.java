050a883c9e38c997b87460cb31bbf1c2a37a227c
Livian1107 <e0134124@u.nus.edu>
2018-02-08 23:40:18 +0800

    add a class Unit

--- src/seedu/addressbook/data/person/Unit.java
+++ src/seedu/addressbook/data/person/Unit.java
@@ -1,4 +1,17 @@
 package seedu.addressbook.data.person;
 
 public class Unit {
+    private final String unit;
+
+    public Unit(String uni){
+        this.unit = uni;
+    }
+
+    public boolean isInvalid(){
+        return unit == null;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
 }
