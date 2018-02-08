c2940c8a90ab3250fce02ee5a5b18a5cb003b390
Livian1107 <e0134124@u.nus.edu>
2018-02-09 00:04:58 +0800

    change the address of example into the corect format

--- test/java/seedu/addressbook/commands/ViewCommandTest.java
+++ test/java/seedu/addressbook/commands/ViewCommandTest.java
@@ -49,7 +49,7 @@ public class ViewCommandTest {
         ReadOnlyPerson stranger = new Person(new Name("me"),
                                              new Phone("123", true),
                                              new Email("some@hey.go", true),
-                                             new Address("nus", false),
+                                             new Address("33, 1 Clementi Road, #02, 345432", false),
                                              new UniqueTagList(Collections.emptySet()));
         List<ReadOnlyPerson> listWithExtraPerson
                 = new ArrayList<ReadOnlyPerson>(listWithAllTypicalPersons);
