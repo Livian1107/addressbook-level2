d6f53ad88672dae39b22f372c239da5720cdc5e0
Livian1107 <e0134124@u.nus.edu>
2018-02-14 17:26:00 +0800

    build a new class Contact

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,4 +1,31 @@
 package seedu.addressbook.data.person;
 
+/**
+ * Represents a Person's Contact in the address book.
+ */
 public class Contact {
+
+    public Contact(){
+    }
+
+    /**
+     * convert the contact information to a string and return the string
+     */
+    public String toString() {
+        throw new UnsupportedOperationException("This method is to be implemented by child classes");
+    }
+
+    /**
+    * judge whether the contacts are the same and return the result of judgement
+    */
+    public boolean equals(Object other) {
+        throw new UnsupportedOperationException("This method is to be implemented by child classes");
+    }
+
+    /**
+     * get the hash code of the contact and return it
+     */
+    public int hashCode() {
+        throw new UnsupportedOperationException("This method is to be implemented by child classes");
+    }
 }
